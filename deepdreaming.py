0c0ef9361c513964510dc18bcbcffe7c6b5b941f
Johannes Bergs <jo@raspython.org>
2015-09-19 13:10:35 +0200

    moving attributes into class

--- deepdreaming.py
+++ deepdreaming.py
@@ -91,19 +91,19 @@ class Dreamer(object):
             PIL.Image.fromarray(np.uint8(frame)).save(output_path())
             frame = nd.affine_transform(frame, [1-s,1-s,1], [h*s/2,w*s/2,0], order=1)
 
-    def make_step(self, net, step_size=1.5, end='inception_4c/output', 
+    def make_step(self, step_size=1.5, end='inception_4c/output', 
                   jitter=32, clip=True, objective=objective_L2):
         """Basic gradient ascent step."""
 
-        src = net.blobs['data'] # input image is stored in Net's 'data' blob
-        dst = net.blobs[end]
+        src = self.net.blobs['data'] # input image is stored in Net's 'data' blob
+        dst = self.net.blobs[end]
 
         ox, oy = np.random.randint(-jitter, jitter+1, 2)
         src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2) # apply jitter shift
                 
-        net.forward(end=end)
+        self.net.forward(end=end)
         objective(dst)  # specify the optimization objective
-        net.backward(start=end)
+        self.net.backward(start=end)
         g = src.diff[0]
         # apply normalized ascent step to the input image
         src.data[:] += step_size/np.abs(g).mean() * g
@@ -111,13 +111,13 @@ class Dreamer(object):
         src.data[0] = np.roll(np.roll(src.data[0], -ox, -1), -oy, -2) # unshift image
                 
         if clip:
-            bias = net.transformer.mean['data']
+            bias = self.net.transformer.mean['data']
             src.data[:] = np.clip(src.data, -bias, 255-bias)  
 
     def deepdream(self, base_img, iter_n=10, octave_n=4, octave_scale=1.4, 
                   end='inception_4c/output', clip=True, **step_params):
         # prepare base images for all octaves
-        octaves = [preprocess(net, base_img)]
+        octaves = [preprocess(self.net, base_img)]
         for i in xrange(octave_n-1):
             octaves.append(nd.zoom(octaves[-1], (1, 1.0/octave_scale,1.0/octave_scale), order=1))
         
@@ -133,7 +133,7 @@ class Dreamer(object):
             src.reshape(1,3,h,w) # resize the network's input image size
             src.data[0] = octave_base+detail
             for i in xrange(iter_n):
-                self.make_step(self.net, end=end, clip=clip, **step_params)
+                self.make_step(end=end, clip=clip, **step_params)
                 
                 # visualization
                 vis = deprocess(self.net, src.data[0])
@@ -147,7 +147,7 @@ class Dreamer(object):
             # extract details produced on the current octave
             detail = src.data[0]-octave_base
         # returning the resulting image
-        return deprocess(net, src.data[0])
+        return deprocess(self.net, src.data[0])
 
 '''
 def make_step(net, step_size=1.5, end='inception_4c/output', 

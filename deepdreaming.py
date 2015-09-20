584afe862ceaf84972e9c69c2bda78d53785e1e0
Johannes Bergs <jo@raspython.org>
2015-09-20 12:06:58 +0200

    cleanup

--- deepdreaming.py
+++ deepdreaming.py
@@ -20,6 +20,19 @@ import os
 # caffe.set_mode_gpu()
 # caffe.set_device(0) # select GPU device if multiple devices exist
 
+def output_path():
+    """ Create an output filename: look into folder dreams,
+        return lowest INTEGER.jpg with leading zeros, e.g. 00020.jpg """
+    # faster with sort
+
+    index=0
+    output_file = "dreams/%06d.jpg"%index
+
+    while os.path.exists(output_file):
+        index += 1
+        output_file = "dreams/%06d.jpg"%index
+
+    return output_file
 
 def create_net(model_file):
     net_fn = os.path.join(os.path.split(model_file)[0], 'deploy.prototxt')
@@ -40,6 +53,7 @@ def create_net(model_file):
 # a couple of utility functions for converting to and from Caffe's input image layout
 def preprocess(net, img):
     return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
+
 def deprocess(net, img):
     return np.dstack((img + net.transformer.mean['data'])[::-1])
 
@@ -54,12 +68,10 @@ class Dreamer(object):
         self.net = net
         self.iterations = iterations
         self.objective = objective_L2
-        # shallow dreams could be implemented here...
 
         self.end = end
 
         if guide_path:
-            #self.end, self.guide_features = self.create_guide(guide_path)
             self.guide_features = self.create_guide(guide_path)
             self.objective = self.objective_guide
 
@@ -77,9 +89,6 @@ class Dreamer(object):
     # use self.end instead of end
     def create_guide(self, guide_path):
         guide = np.float32(PIL.Image.open(guide_path))
-        # why is an end set for the guided dream? try result without
-        # this makes the dream more shallow
-        #end = 'inception_3b/output'
         h, w = guide.shape[:2]
         src, dst = net.blobs['data'], net.blobs[self.end]
         src.reshape(1,3,h,w)
@@ -87,7 +96,6 @@ class Dreamer(object):
         self.net.forward(end=self.end)
 
         return dst.data[0].copy()
-        #return end, dst.data[0].copy()
 
     def iterated_dream(self):
         self.net.blobs.keys()
@@ -162,38 +170,11 @@ class Dreamer(object):
         return deprocess(self.net, src.data[0])
 
 
-
-def output_path():
-    # faster with sort
-
-    index=0
-    output_file = "dreams/%06d.jpg"%index
-
-    while os.path.exists(output_file):
-        index += 1
-        output_file = "dreams/%06d.jpg"%index
-
-    return output_file
-
-
-# enable --help functionality
-
-# how does the "impressionist" style work?
-
-# shallow layers create textured images:
-# why is this set to reduce and not output?
-# deepdream(net, img, end='inception_3b/5x5_reduce')
-# why is this shallow?
-# why reduce and not output?
-
-# 3b is more shallow than 4c...
-
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--source', nargs='?', const='sky_1024.jpg', default='sky_1024.jpg')
-    #parser.add_argument('-d', '--depth', nargs='?', const='inception_4c/output', default='inception_4c/output')
     parser.add_argument('-g', '--guide', nargs='?', default=None)
     parser.add_argument('-i', '--iterations', nargs='?', type=int, const=1, default=1)
     parser.add_argument('-m', '--model', nargs='?', metavar='int', type=int,
@@ -205,18 +186,16 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--type', nargs='?', metavar='int', type=int,
                                     choices=xrange(1, 10), help='layer type 1..6',
                                     const=4, default=4)
-    # add -d = depth
+
 
     models_base = '../caffe/models'
-    #model_default = '../caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel'
+
     models = ('bvlc_googlenet/bvlc_googlenet.caffemodel',
                     'bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
                     'bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel',
                     'finetune_flickr_style/finetune_flickr_style.caffemodel',
                     'bvlc_alexnet/bvlc_alexnet.caffemodel')
-    # add depth
 
-    # MODELS: layer names seem to be off
 
     # move argument parsing into function
 
@@ -227,8 +206,6 @@ if __name__ == "__main__":
     layer_types = ['1x1', '3x3', '5x5', 'output', '5x5_reduce', '3x3_reduce']
 
     layer = 'inception_' + numbering[args.depth-1] + '/' + layer_types[args.type-1]
-
-    # off for layer test
     
     dreamer = Dreamer(net=net, source_path=args.source, 
                                    iterations=args.iterations, end=layer, 
@@ -244,99 +221,8 @@ if __name__ == "__main__":
 
     # write in Readme and post -d 2 for guided
     # write in Readme and post -d 2 -t 5 for shallow
+    # (generally, add some sample input)
 
-    # 18 layer types...
-    #numbering = ['3a', '3b', '4a', '4b', '4c', '4d', '4e', '5a', '5b']
-    #layer_types = ['/output', '/5x5_reduce']
-
-    # test it with one of every output type, not all permutations!
-
-    # it seems as if we have:
-
-
-    # more easy:
-    #   make a set from all tops or bottoms; does sorting work?
-    '''
-    for t in types:
-        end = 'inception_4c/' + t
-        dreamer = Dreamer(net=net, source_path=args.source, 
-                               iterations=args.iterations, end=end, 
-                               guide_path=args.guide)
-        dreamer.iterated_dream()
-    '''
-
-    '''
-    for n in numbering:
-        for t in types:
-            end = 'inception_' + n + '/' + t
-            dreamer = Dreamer(net=net, source_path=args.source, 
-                                   iterations=args.iterations, end=end, 
-                                   guide_path=args.guide)
-            dreamer.iterated_dream()
-    '''
-
-
-    # 1x1 is valid
-
-    # a layer has to be bottom (or top), not only name
-
-    # maybe the other layer types are cool, too?
-    # try relu_5x5_reduce
-    #   relu_5x5
-    #   inception_3a/1x1
-    #   3b/relu_pool_proj
-    '''
-    layer_types = {}
-
-    index = 0
-    for n in numbering:
-        for t in types:
-            layer_types[index] = n + t
-            index += 1
-    '''
-
-    #print "len layer_types: " + str(len(layer_types))
-
-    # first inception_ "inception_3a/1x1"
-    # last inception_3a/output
-    # then inception_3b/1x1
-    # last inception_3b/output
-    # then inception_4a/1x1
-    # passes inception_4a/5x5_reduce
-    # last inception_4a/output
-    # 4b, 4c ends with inception_4c/output
-    # 4d, 4e
-    # ends with inception_5b/output
-
-    # test depth:
-    #   guided dream with shallow depth
-    #   normal dream with guided dream depth
-    #   find other values for depth
-    #   why is shallow 5x5_reduce?
-
-    # 00030: deep dream to 4c depth
-    # 00031: deep dream to inception_3b/5x5_reduce depth
-    # 00032: deep dream to inception_3b/output depth
-    # 00033: deep dream to inception_3a/output depth
-
-    # 00034: deep dream to inception_3a/output depth no guide
-    # 00035: deep dream to inception_3a/5x5_reduce depth no guide
-    # 00036: deep dream to inception_5b/output depth no guide
-    # 00037: deep dream to inception_5b/5x5_reduce depth no guide
-    # 00038: deep dream to inception_4c/output depth no guide
-    # 00039: deep dream to inception_4c/5x5_reduce depth no guide
-
-    # the reduce layer exists for every output layer: what is the difference between
-    #   reduce and output? try non-guided for different depths
-
-
-    # sample input:
-    # normal dream
-    # python deepdreaming.py start=raspberry_pi_1024.jpg guide=None iterations=None
-    # iterative dream
-    # python deepdreaming.py start=raspberry_pi_1024.jpg guide=None iterations=2
-    # guided dream
-    # python deepdreaming.py start=raspberry_pi_1024.jpg guide=mops_1024.jpg iterations=None
 
     # can we fuse iteratvie and normal dream
     # plus, iterative guided dream???
